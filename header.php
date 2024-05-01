f0ebdc44f53ced70c84f0d9d9caf8eb0d76aa5d2
almamun469 <almamundewan469@gmail.com>
2024-05-01 11:24:24 +0600

    done

---
+++ header.php
@@ -0,0 +1,15 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h1 id="main_title">CRUD APPLICATION IN PHP</h1>
+  <div class="container">
